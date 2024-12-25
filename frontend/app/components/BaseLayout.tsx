1362893e4adacb91f0c18fcc385198becd9e1c65
moeko-iwai <moepyxxxxxx@gmail.com>
2024-12-25 18:59:08 +0900

    update: logout

--- frontend/app/components/BaseLayout.tsx
+++ frontend/app/components/BaseLayout.tsx
@@ -1,15 +1,47 @@
 import type { FC, PropsWithChildren } from "react";
 import { Logo } from "./Logo";
 import { Title } from "~/components/Title";
+import { Button } from "./ui/button";
+import { useNavigate } from "@remix-run/react";
+import { useToast } from "~/hooks/use-toast";
 
 type Props = PropsWithChildren & {
   title: string;
 };
 
 export const BaseLayout: FC<Props> = ({ children, title }) => {
+  const navigate = useNavigate();
+  const { toast } = useToast();
+
+  const handleLogout = async () => {
+    const response = await fetch(`${import.meta.env.VITE_API_URL}/logout`, {
+      method: "POST",
+      headers: new Headers({
+        "Content-Type": "application/json",
+      }),
+      credentials: "include",
+    });
+    if (!response.ok) {
+      toast({
+        description: "ログアウトに失敗しました・・・",
+      });
+      return;
+    }
+    toast({
+      description: "ログアウトしました",
+    });
+    navigate("/signin");
+  };
   return (
     <div className="min-h-dvh pb-10">
-      <Logo />
+      <div className="fixed top-3 px-3 w-full flex justify-between items-center">
+        <Logo />
+        <Button variant="ghost" onClick={handleLogout}>
+          ログアウト
+          <i className="fa-solid fa-right-from-bracket" />
+        </Button>
+      </div>
+
       <Title>{title}</Title>
       <div className="px-4">{children}</div>
     </div>
--- frontend/app/components/Logo.tsx
+++ frontend/app/components/Logo.tsx
@@ -2,9 +2,5 @@ import { Link } from "@remix-run/react";
 import type { FC } from "react";
 
 export const Logo: FC = () => {
-  return (
-    <div className="fixed top-4 left-4">
-      <Link to="/">ていねいに食べよう</Link>
-    </div>
-  );
+  return <Link to="/">ていねいに食べよう</Link>;
 };
